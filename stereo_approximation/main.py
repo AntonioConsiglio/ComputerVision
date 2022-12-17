59c071cf17d3caf3c7c84b365be1c664d1597f7d
Antonio Consiglio <77753494+AntonioConsiglio@users.noreply.github.com>
2022-12-17 10:50:35 +0100

    s

--- stereo_approximation/main.py
+++ stereo_approximation/main.py
@@ -1,18 +1,12 @@
-# import os
-# print(os.getcwd())
-# os.chdir(__file__.rsplit("\\",1)[0])
-# print(os.getcwd())
-# from utils import feature_matcher,load_images,show_kpoint
+import os
+print(os.getcwd())
+os.chdir(__file__.rsplit("\\",1)[0])
+print(os.getcwd())
+from utils import feature_matcher,load_images,show_kpoint
 
-# image1,image2 = load_images("./images")
+image1,image2 = load_images("./images")
 
-# pt1,pt2 = feature_matcher(image1,image2,nfeatures=300)
+pt1,pt2 = feature_matcher(image1,image2,nfeatures=300)
 
-# show_kpoint([image1,image2],[pt1,pt2])
+show_kpoint([image1,image2],[pt1,pt2])
 
-
-lista1 = [1,3]
-lista2 = [1,3]
-
-if any(lista1 == lista2):
-    print("OKK")
